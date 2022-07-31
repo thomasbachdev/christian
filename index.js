3d58000c2c49238e636acc4f74036fb7f78bc806
Thomas Bach <thomasbach38@gmail.com>
2022-08-01 00:11:06 +0200

    Ratio working

--- index.js
+++ index.js
@@ -3,13 +3,30 @@
  * Github : https://github.com/thomasbachdev
  */
 
-const { Client, GatewayIntentBits } = require("discord.js");
-const auth = require("./auth.json");
+const { Client, GatewayIntentBits } = require('discord.js');
+const auth = require('./auth.json');
+const pref = require('./preferences.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
-client.once("ready", () => {
-  console.log("Initialization done.");
+client.once('ready', () => {
+  console.log('Ready !');
+});
+
+client.on('messageCreate', (message) => {
+  let x = Math.random();
+  if (x <= pref['ratio-prob']) {
+    message.reply('ratio', ).then((reply) => {
+      reply.react('👍');
+    });
+  }
 });
 
 client.login(auth.token);
